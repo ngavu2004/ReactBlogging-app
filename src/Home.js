0e76b9b77d37ae2cc9bd9c00749d0636e56904ca
Nga Vu <vuthiquynhngahvn@gmail.com>
2023-03-06 11:08:11 -0700

    return to base code

--- src/Home.js
+++ src/Home.js
@@ -10,10 +10,6 @@ const Home = () => {
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
-            {/* // When we need to use function  */}
-            {/* // with parameter, we cannot use  */}
-            {/* // OnClick={\handleClick('para')} right away bc it will invoke the function immediately when the program run */}
-            <button onClick={(e) => {handleClickAgain('Babygirl',e)}}>Click me again</button>
         </div>
     );
 }
