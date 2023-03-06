fc6604b0bd6a02e376dc542c23026afc67d69457
Nga Vu <vuthiquynhngahvn@gmail.com>
2023-03-06 11:47:50 -0700

    Props example

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,6 @@
 const BlogList = ({blogs, title}) => {
-    const blogs = props.blogs;
-    const title = props.title;
+    // const blogs = props.blogs;
+    // const title = props.title;
     return (  
         <div className="blog-list">
             <h2>{title}</h2>
