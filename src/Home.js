215169b6ba5f412354b7e2ec2195151f61a7b60d
Nga Vu <vuthiquynhngahvn@gmail.com>
2023-03-06 10:35:13 -0700

    State hook to change value actively

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
 
-    let name = 'mario';
+    // let name = 'mario';
+    // This is reactive
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
     const handleClick = () => {
-        name = 'luigi';
-        console.log(name);
+        setName('luigi');
+        setAge(30);
     }
     return (  
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name}</p>
+            <p>{name} is {age} years old</p>
             {/* When click doesnt make the p tag content change because the name variable is not active. By active it means react watches for its changes*/}
             <button onClick={handleClick}>Click me</button>
             
