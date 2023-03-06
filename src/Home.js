dc6443b6e47c68ee18a8e63ccb2fcd6fe92def1e
Nga Vu <vuthiquynhngahvn@gmail.com>
2023-03-06 10:30:22 -0700

    State hook

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,17 @@
 const Home = () => {
-    const handleClick = () => {
-        console.log('Hello cookies');
-    }
 
-    const handleClickAgain = (name,e) => {
-        console.log('hello' + name);
+    let name = 'mario';
+    const handleClick = () => {
+        name = 'luigi';
+        console.log(name);
     }
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
+            {/* When click doesnt make the p tag content change because the name variable is not active. By active it means react watches for its changes*/}
             <button onClick={handleClick}>Click me</button>
-            {/* // When we need to use function  */}
-            {/* // with parameter, we cannot use  */}
-            {/* // OnClick={\handleClick('para')} right away bc it will invoke the function immediately when the program run */}
-            <button onClick={(e) => {handleClickAgain('Babygirl',e)}}>Click me again</button>
+            
         </div>
     );
 }
