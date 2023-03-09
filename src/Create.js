56e7814298da69078c42d73994575f51bc7fa977
Nga Vu <vuthiquynhngahvn@gmail.com>
2023-03-09 13:50:31 -0700

    Control the input fields

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
 const Create = () => {
+    const [title, setTitle] = useState('hello');
+    const [body, setBody] = useState('hello');
+    const [author, setAuthor] = useState('mario');
     return (  
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form action="">
+                <label>Blog title:</label>
+                <input 
+                    type="text" 
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea 
+                    value={body}
+                    required
+                    onChange={(e) => setBodye.target.value}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">yoshi</option>                    
+                </select>
+                <button>Add blog</button>
+                <p>{title}</p>
+            </form>
         </div>
     );
 }
