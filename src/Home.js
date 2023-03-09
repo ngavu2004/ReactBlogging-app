693a5f598450c73137d01757ac252d3f8f07a4c4
Nga Vu <vuthiquynhngahvn@gmail.com>
2023-03-08 17:24:51 -0700

    Fetch data with json server

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }]);
+    const [blogs, setBlogs] = useState(null);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -15,8 +12,13 @@ const Home = () => {
     const [name, setName] = useState('mario');
 
     useEffect(() => {
-        console.log('use effect ran');
-        console.log(name);
+        fetch("http://localhost:8000/blogs")
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+            })
     }, [name])
 
     return (  
